79834959f69631bf0d48042d36d9d1f8db041149
sanal dath <149995346+SanalDath@users.noreply.github.com>
2024-09-11 13:42:12 +0530

    created server

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+
+app.use(cors());
+
+const PORT = 8000 || process.env.PORT;
+
+app.listen(PORT, () => console.log(`The server is running on ${PORT} sucessfuly`));
